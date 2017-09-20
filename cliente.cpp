4372d29e2fe16587a6ba7dd2d240b82cb2ad7dcb
Maesly <nadieska23@outlook.es>
2017-09-20 03:35:22 -0600

    Actualizaciones del cliente

--- cliente.cpp
+++ cliente.cpp
@@ -59,9 +59,9 @@ void Cliente::ejecutar() {
         cout <<"Error al crear el socket cliente";
         exit(0);
     }
-    cout<<"Escriba # para acabar la comunicacion" << endl;
-    cout<< "\t\t\t [s] para empezar";
-    cin>>a;
+    //cout<<"Escriba # para acabar la comunicacion" << endl;
+    //cout<< "\t\t\t [s] para empezar";
+   // cin>>a;
 
     cout << "Socket creado"<< endl;
     server_addr.sin_family = AF_INET;               //asinar a la estructura sockaddr un dominio, ip y puerto
@@ -71,37 +71,30 @@ void Cliente::ejecutar() {
     if(connect(cliente,(struct sockaddr *) &server_addr,sizeof(server_addr))==0)
         cout << "Conexion con el servidor\n"<< inet_ntoa(server_addr.sin_addr)<<endl;
 
-    cout << "Esperando confirmacion del servidor "<< endl;
-    recv(cliente,buffer,bufsize,0);
 
-    cout<< "Respuesta recibida"<< buffer;
-    cout<< "\n Poner * al final de cada mensaje \n Escriba # para terminar la conexion\n";
+    while (!salir){
+        cout << "Oprima 1 para insertar elemento\n";
+        cout << "Oprima 2 para buscar elemento\n";
+        cout << "Oprima 3 para eliminar elemento\n";
+        cin>>buffer;
 
-    do{
-        cout<< "Escribir mensaje: ";
-        do{
-            cin>> buffer;
+        if(*buffer == '1'){
             send(cliente,buffer,bufsize,0);
-            if(*buffer == '#'){
-                send(cliente,buffer,bufsize,0);
-                *buffer = '*';
-                salir = true;
+            cout << *buffer;
+        }
+        else if(*buffer == '2'){
+            send(cliente,buffer,bufsize,0);
+            cout << *buffer;
+        }
+        else if(*buffer == '3'){
+            send(cliente,buffer,bufsize,0);
+            cout << *buffer;
+        }
+        else{
+            send(cliente,buffer,bufsize,0);
+        }
+        }
+        //strcpy(buffer,"HOLIS");
 
-            }
-        }while(*buffer != 42);
-        cout<< "Mensaje recibido: ";
-        do{
-            recv(cliente,buffer,bufsize,0);
-            cout<< buffer<<" ";
-            if(*buffer ==  '#'){
-                *buffer = '*';
-                salir = true;
-
-            }
-        }while(*buffer != 42);
-        cout<<endl;
-    }while(!salir);
-    cout<< "Conexion terminada. Programa finalizado\n\n";
-    close(cliente);
-    return;
+        //exit(0);
 }
